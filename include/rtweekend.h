e229c574be31288100530929f1b24f9ddbbb3680
Christian Tarrasch <christiantarrasch@gmail.com>
2025-12-27 23:16:53 -0600

    feat: add random_double utility functions and cstdlib include

--- include/rtweekend.h
+++ include/rtweekend.h
@@ -2,6 +2,7 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -18,6 +19,20 @@ const double pi = 3.141519;
 
 inline double degrees_to_radians(double degrees) {
   return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+
+  // Returns random int from [0,1)
+
+  return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+
+  // Returns random int from [min, max)
+
+  return min + (max - min) * random_double();
 }
 
 #include "color.h"
